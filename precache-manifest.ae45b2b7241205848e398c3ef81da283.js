653e56061128380644f972d51b76e7e1faf1bc16
Ethan Brooks <ethanabrooks@gmail.com>
2020-04-06 13:39:00 -0400

    Updates

---
+++ precache-manifest.ae45b2b7241205848e398c3ef81da283.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1a7ed45e70062ddcc428a712bc8083f",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "f77d46c68f1e3d04ec9c",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6210bf1ded25448980ff",
+    "url": "/my-app/static/js/2.2594f893.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.2594f893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f77d46c68f1e3d04ec9c",
+    "url": "/my-app/static/js/main.7fe8c03b.chunk.js"
+  },
+  {
+    "revision": "293656d52bf581d13418",
+    "url": "/my-app/static/js/runtime-main.9e7aca70.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
